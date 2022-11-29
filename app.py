69fc59dbf29f61028da73aaba1a5acc941d3a3c4
TrinidadMartiG <trinidad.martig@gmail.com>
2022-11-29 21:15:31 +0000

    added user endpoints

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS 
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy 
-from models import db
+from models import db, User, Media, Text
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///final-project.db"
@@ -10,11 +10,30 @@ db.init_app(app)
 CORS(app)
 Migrate(app, db)
 
+@app.route("/")
+def hello():
+    return "Hello World!"
 
+@app.route("/users", methods=['GET'])
+def get_all_users():
+    all_user = User.query.all()
+    serialize_list = list(map(lambda user: user.serialize(), all_user)) 
+    response_body = {
+        "msg":  "Get /user response was successful"
+    }
 
+    return jsonify(serialize_list), 201
 
+@app.route("/create_user", methods=["POST"])
+def create_user():
+    user = User()
+    user.name = request.json.get("name")
+    user.email = request.json.get("email")
+    user.password = request.json.get("password")
+    user.is_active = request.json.get("is_active")
+    db.session.add(user)
+    db.session.commit()
+    return 'User registered',202
 
-
-
-
-app.run(host="localhost", port=8080)
+if __name__ == "__main__":
+    app.run(host="localhost", port=8080, debug=True)
--- models.py
+++ models.py
@@ -2,25 +2,71 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
 class User(db.Model):
-    __tablename__ = "users"
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False, )
-    password = db.Column(dbs.String(50), nullable=False)
+    name = db.Column(db.String(50), nullable=False, unique=False)
+    email = db.Column(db.String(100), nullable=False, unique=True )
+    password = db.Column(db.String(50), nullable=False, unique=False)
+    is_active = db.Column(db.Boolean(), nullable=False, unique=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "password": self.password,
+            "is_active": self.is_active
+        }
 
 class Media(db.Model):
-    __tablename__ = "medias"
+    __tablename__ = "media"
     id = db.Column(db.Integer, primary_key=True)
-    user.id= db.Column(db.Integer, db.ForeignKey('users_id'), nullable=False)
-    mainTitleVideo = db.Column(db.string(500))
+    user_id= db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    mainTitleVideo = db.Column(db.String(500))
+    thirdSectionLeftConceptOneImg = db.Column(db.String(500))
+    thirdSectionRightConceptTwoImg = db.Column(db.String(500))
+    thirdSectionLeftConceptThreeImg = db.Column(db.String(500))
+
 
 class Text(db.Model):
-    __tablename__ = "texts"
+    __tablename__ = "text"
     id = db.Column(db.Integer, primary_key=True)
-    user.id= db.Column(db.Integer, db.ForeignKey('users_id'), nullable=False)
-    mainTitle = db.Column(db.string(200), nullable=False)
-    mainDescription = db.Column(db.string(200))
-    secondSectionMainTitle = db.Column(db.string(200), nullable=False)
-    secondSectionDescription = db.Column(db.string(200))
+    user_id= db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    mainTitle = db.Column(db.String(200), nullable=False)
+    mainDescription = db.Column(db.String(200))
+
+    secondSection_MainTitle = db.Column(db.String(200), nullable=False)
+    secondSection_Description = db.Column(db.String(200))
+    secondSection_ConceptOne = db.Column(db.String(200))
+    secondSection_ConceptTwo = db.Column(db.String(200))
+    secondSection_ConceptThree = db.Column(db.String(200))
+    secondSection_ConceptFour = db.Column(db.String(200))
+    secondSection_ConceptFive = db.Column(db.String(200))
+    secondSection_ConceptSix = db.Column(db.String(200))
+
+    thirdSection_MainTitle = db.Column(db.String(200), nullable=False)
+    thirdSection_Description = db.Column(db.String(200))
+    thirdSection_LeftConceptOne_BlueHighlightText = db.Column(db.String(200))
+    thirdSection_LeftConceptOne_Title = db.Column(db.String(200))
+    thirdSection_LeftConceptOne_Description = db.Column(db.String(200))
+    thirdSection_RightConceptTwo_BlueHighlightText = db.Column(db.String(200))
+    thirdSection_RightConceptTwo_Title = db.Column(db.String(200))     
+    thirdSection_RightConceptTwo_Description = db.Column(db.String(200))
+    thirdSection_LeftConceptThree_BlueHighlightText = db.Column(db.String(200))
+    thirdSection_LeftConceptThree_Title = db.Column(db.String(200))
+    thirdSection_LefttConceptThree_Description = db.Column(db.String(200))
+
+    fourthSection_TestimonialsMainTitle = db.Column(db.String(200), nullable=False)
+    fourthSection_TestimonialsDescription = db.Column(db.String(200))
+    fourthSection_CardOneText = db.Column(db.String(200))
+    fourthSection_CardOne_Person = db.Column(db.String(200))
+    fourthSection_CardTwoText = db.Column(db.String(200))
+    fourthSection_CardTwo_Person = db.Column(db.String(200))
+    fourthSection_CardThreeText = db.Column(db.String(200))
+    fourthSection_CardThree_Person = db.Column(db.String(200))
+
