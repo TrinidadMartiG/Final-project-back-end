e3d27b9acc3c10f60ad40c73f65f83a8403a7a1c
TrinidadMartiG <trinidad.martig@gmail.com>
2022-12-05 18:53:12 +0000

    added PUT method to modify row data

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, request, render_template
-from flask_cors import CORS 
+from flask_cors import CORS
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy 
-from models import db, User, Media, Text
+from flask_sqlalchemy import SQLAlchemy
+from models import db, User, Media, Text_FirstSection, Text_SecondSection, Text_ThirdSection, Text_FourthSection
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///final-project.db"
@@ -10,19 +10,22 @@ db.init_app(app)
 CORS(app)
 Migrate(app, db)
 
+
 @app.route("/")
 def hello():
     return "Hello World!"
 
+
 @app.route("/users", methods=['GET'])
 def get_all_users():
     all_user = User.query.all()
-    serialize_list = list(map(lambda user: user.serialize(), all_user)) 
+    serialize_list = list(map(lambda user: user.serialize(), all_user))
     response_body = {
         "msg":  "Get /user response was successful"
     }
 
     return jsonify(serialize_list), 201
+
 
 @app.route("/create_user", methods=["POST"])
 def create_user():
@@ -30,10 +33,46 @@ def create_user():
     user.name = request.json.get("name")
     user.email = request.json.get("email")
     user.password = request.json.get("password")
-    user.is_active = request.json.get("is_active")
+   # user.is_active = request.json.get("is_active")
     db.session.add(user)
     db.session.commit()
-    return 'User registered',202
+    return 'User registered', 202
+
+#routes for first section
+@app.route("/send_data_firstsection", methods=["POST"])
+def send_data_firstsection():
+    data = Text_FirstSection()
+    data.id = request.json.get("id")
+    data.user_id = request.json.get("user_id")
+    data.mainTitle = request.json.get("mainTitle")
+    data.mainDescription = request.json.get("mainDescription")
+    db.session.add(data)
+    db.session.commit()
+    return 'data sended', 202
+
+
+@app.route("/update_data_firstsection/<int:user>", methods=["PUT"])
+def update_data_firstsection(user=None):
+    if user is not None:
+        print(user)
+        text_firstsection = Text_FirstSection.query.filter_by(user_id=user)
+        print(text_firstsection)
+        text_firstsection.mainTitle = request.json.get("mainTitle")
+        text_firstsection.mainDescription = request.json.get("mainDescription")
+        db.session.commit()
+        return 'data updated', 202
+
+
+@app.route("/get_data_firstsection", methods=["GET"])
+def get_data_firstsection():
+    all_data = Text_FirstSection.query.all()
+    serialize_list = list(
+        map(lambda text_firstsection: text_firstsection.serialize(), all_data))
+    response_body = {
+        "msg":  "Get /data response was successful"
+    }
+    return jsonify(serialize_list), 201
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
--- models.py
+++ models.py
@@ -2,13 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False, unique=False)
-    email = db.Column(db.String(100), nullable=False, unique=True )
+    email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(50), nullable=False, unique=False)
-    is_active = db.Column(db.Boolean(), nullable=False, unique=False)
+    is_active = db.Column(db.Boolean(), default=True,
+                          nullable=False, unique=False)
 
     def __repr__(self):
         return '<User %r>' % self.email
@@ -22,25 +24,54 @@ class User(db.Model):
             "is_active": self.is_active
         }
 
+
 class Media(db.Model):
     __tablename__ = "media"
     id = db.Column(db.Integer, primary_key=True)
-    user_id= db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     mainTitleVideo = db.Column(db.String(500))
     thirdSectionLeftConceptOneImg = db.Column(db.String(500))
     thirdSectionRightConceptTwoImg = db.Column(db.String(500))
     thirdSectionLeftConceptThreeImg = db.Column(db.String(500))
 
+    def __repr__(self):
+        return '<User %r>' % self.user_id
 
-class Text(db.Model):
-    __tablename__ = "text"
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "mainTitleVideo": self.mainTitleVideo,
+            "thirdSectionLeftConceptOneImg": self.thirdSectionLeftConceptOneImg,
+            "thirdSectionRightConceptTwoImg" : self.thirdSectionRightConceptTwoImg,
+            "thirdSectionLeftConceptThreeImg": self. thirdSectionLeftConceptThreeImg
+        }
+
+
+class Text_FirstSection(db.Model):
+    __tablename__ = "text_firstsection"
     id = db.Column(db.Integer, primary_key=True)
-    user_id= db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    mainTitle = db.Column(db.String(200), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    mainTitle = db.Column(db.String(200))
     mainDescription = db.Column(db.String(200))
 
-    secondSection_MainTitle = db.Column(db.String(200), nullable=False)
+    def __repr__(self):
+        return '<User %r>' % self.user_id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "er_idus": self.user_id,
+            "mainTitle": self.mainTitle,
+            "mainDescription": self.mainDescription
+        }
+
+
+class Text_SecondSection(db.Model):
+    __tablename__ = "text_secondsection"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    secondSection_MainTitle = db.Column(db.String(200))
     secondSection_Description = db.Column(db.String(200))
     secondSection_ConceptOne = db.Column(db.String(200))
     secondSection_ConceptTwo = db.Column(db.String(200))
@@ -49,19 +80,64 @@ class Text(db.Model):
     secondSection_ConceptFive = db.Column(db.String(200))
     secondSection_ConceptSix = db.Column(db.String(200))
 
-    thirdSection_MainTitle = db.Column(db.String(200), nullable=False)
+    def __repr__(self):
+        return '<User %r>' % self.user_id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "secondSection_MainTitle": self.secondSection_MainTitle,
+            "secondSection_Description": self.secondSection_Description,
+            "secondSection_ConceptOne": self.secondSection_ConceptOne,
+            "secondSection_ConceptTwo": self.secondSection_ConceptTwo,
+            "secondSection_ConceptThree": self.secondSection_ConceptThree,
+            "secondSection_ConceptFour": self.secondSection_ConceptFour,
+            "secondSection_ConceptFive": self.secondSection_ConceptFive,
+            "secondSection_ConceptSix": self.secondSection_ConceptSix
+        }
+
+class Text_ThirdSection(db.Model):
+    __tablename__ = "text_thirdsection"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    thirdSection_MainTitle = db.Column(db.String(200))
     thirdSection_Description = db.Column(db.String(200))
     thirdSection_LeftConceptOne_BlueHighlightText = db.Column(db.String(200))
     thirdSection_LeftConceptOne_Title = db.Column(db.String(200))
     thirdSection_LeftConceptOne_Description = db.Column(db.String(200))
     thirdSection_RightConceptTwo_BlueHighlightText = db.Column(db.String(200))
-    thirdSection_RightConceptTwo_Title = db.Column(db.String(200))     
+    thirdSection_RightConceptTwo_Title = db.Column(db.String(200))
     thirdSection_RightConceptTwo_Description = db.Column(db.String(200))
     thirdSection_LeftConceptThree_BlueHighlightText = db.Column(db.String(200))
     thirdSection_LeftConceptThree_Title = db.Column(db.String(200))
     thirdSection_LefttConceptThree_Description = db.Column(db.String(200))
 
-    fourthSection_TestimonialsMainTitle = db.Column(db.String(200), nullable=False)
+    def __repr__(self):
+        return '<User %r>' % self.user_id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "thirdSection_MainTitle": self.thirdSection_MainTitle,
+            "thirdSection_Description": self.thirdSection_Description,
+            "thirdSection_LeftConceptOne_BlueHighlightText": self.thirdSection_LeftConceptOne_BlueHighlightText,
+            "thirdSection_LeftConceptOne_Title": self.thirdSection_LeftConceptOne_Title,
+            "thirdSection_LeftConceptOne_Description": self.thirdSection_LeftConceptOne_Description,
+            "thirdSection_RightConceptTwo_BlueHighlightText": self.thirdSection_RightConceptTwo_BlueHighlightText,
+            "thirdSection_RightConceptTwo_Title": self.thirdSection_RightConceptTwo_Title,
+            "thirdSection_RightConceptTwo_Description": self.thirdSection_RightConceptTwo_Description,
+            "thirdSection_LeftConceptThree_BlueHighlightText": self.thirdSection_LeftConceptThree_BlueHighlightText,
+            "thirdSection_LeftConceptThree_Title": self.thirdSection_LeftConceptThree_Title,
+            "thirdSection_LefttConceptThree_Description": self.thirdSection_LefttConceptThree_Description
+        }
+
+class Text_FourthSection(db.Model):
+    __tablename__ = "text_fourthsection"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    fourthSection_TestimonialsMainTitle = db.Column(db.String(200))
     fourthSection_TestimonialsDescription = db.Column(db.String(200))
     fourthSection_CardOneText = db.Column(db.String(200))
     fourthSection_CardOne_Person = db.Column(db.String(200))
@@ -70,3 +146,19 @@ class Text(db.Model):
     fourthSection_CardThreeText = db.Column(db.String(200))
     fourthSection_CardThree_Person = db.Column(db.String(200))
 
+    def __repr__(self):
+        return '<User %r>' % self.user_id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "fourthSection_TestimonialsMainTitle": self.fourthSection_TestimonialsMainTitle,
+            "fourthSection_TestimonialsDescription": self.fourthSection_TestimonialsDescription,
+            "fourthSection_CardOneText": self.fourthSection_CardOneText,
+            "fourthSection_CardOne_Person": self.fourthSection_CardOne_Person,
+            "fourthSection_CardTwoText": self.fourthSection_CardTwoText,
+            "fourthSection_CardTwo_Person": self.fourthSection_CardTwo_Person,
+            "fourthSection_CardThreeText": self.fourthSection_CardThreeText,
+            "fourthSection_CardThree_Person": self.fourthSection_CardThree_Person
+        }   
